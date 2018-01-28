3c3e0f97d5a15944cbbfaeeef8cad6f4e4507f39
Roberto Arruda <robertoadearruda@gmail.com>
2018-01-28 15:28:52 -0200

    Migrate tabela users

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Hash;
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->email,
+        'password' => Hash::make('pass'),
+        'status' => rand(1, 0),
+    ];
+});
